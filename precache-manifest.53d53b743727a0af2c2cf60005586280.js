384893e9c26783eeafadfffd230c2b9cca7eb660
Dmitriy Guminichenko <snitchrus66@gmail.com>
2020-04-29 20:04:16 +0500

    rainbow

---
+++ precache-manifest.53d53b743727a0af2c2cf60005586280.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a03444f69c8cb655c6",
+    "url": "/css/about.ddc7613e.css"
+  },
+  {
+    "revision": "dae330941dd1978f5084",
+    "url": "/css/chunk-vendors.eb3da97e.css"
+  },
+  {
+    "revision": "0527bde89f59bc7a5a305021b78acf2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2a03444f69c8cb655c6",
+    "url": "/js/about.5228593b.js"
+  },
+  {
+    "revision": "0e1c263785dd002103a9",
+    "url": "/js/app.13dab41d.js"
+  },
+  {
+    "revision": "dae330941dd1978f5084",
+    "url": "/js/chunk-vendors.03a010f5.js"
+  },
+  {
+    "revision": "542a7ac6b67d5d744cdef77e14853459",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
